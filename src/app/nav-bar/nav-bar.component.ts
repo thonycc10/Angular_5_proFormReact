314d17f8c8834833c443af3f8a4c76b948758d59
Anthony Carrasco <anthony.carrasco@attlaris.pe>
2018-03-13 16:39:34 -0500

    Formulario Reactivos

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -11,11 +11,13 @@ export class NavBarComponent implements OnInit {
   post: any;
   description = '';
   name = '';
+  titleAlert = 'Este controlador es Requerido';
+  alertArea = 'Es Requerido mas de 30 caracteres y menos de 500 caracteres';
   private loggedIn = false;
   constructor(private fb: FormBuilder) {
     this.rForm = fb.group({
       'name': [null, Validators.required],
-      'descripcion': [null, Validators.compose([Validators.required, Validators.minLength(30), Validators.maxLength(500)])],
+      'description': [null, Validators.compose([Validators.required, Validators.minLength(30), Validators.maxLength(500)])],
       'validate' : ''
     });
   }
@@ -26,7 +28,20 @@ export class NavBarComponent implements OnInit {
   toggleDisplay() {
     this.loggedIn = !this.loggedIn;
   }
-  ngOnInit() {
-  }
 
+  ngOnInit() {
+
+    this.rForm.get('validate').valueChanges.subscribe(
+      (validate) => {
+        if (validate == '1') {
+          this.rForm.get('name').setValidators([Validators.required, Validators.minLength(3)]);
+          this.titleAlert = 'Usted necesita especificar mas de 3 caracteres';
+        } else {
+          this.rForm.get('name').setValidators(Validators.required);
+          this.titleAlert = 'Este controlador es Requerido';
+        }
+        this.rForm.get('name').updateValueAndValidity();
+      }
+    )
+  }
 }
